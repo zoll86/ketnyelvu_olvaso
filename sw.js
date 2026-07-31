6dddcb7e9f403796f36bafb1c4c13e81f7f78e87
zoll86 <rozsa.zolee@gmail.com>
2026-07-31 09:52:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - ikonok, manifest, betűtípusok: cache először (ezek ritkán változnak).
    - fordítókérések: soha nem cache-elve. */
 
-const VER   = 'olvaso-v88';
+const VER   = 'olvaso-v89';
 const SHELL = [
   './',
   './index.html',
